d865685f888cf2a51254432917e9b293f978d3ab
artjom <artemdobrom@gmail.com>
2023-08-08 19:22:31 +0300

    CourseOneIsDone

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.util.Arrays;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Проверка на полиндром");
@@ -18,5 +14,5 @@ public class Main {
         } else {
             System.out.println("это не палиндром");
         }
-        }
     }
+}
